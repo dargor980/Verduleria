0f3536e379538daa8575a2ca284cf1bf46694a20
Braulio Argandoña <braulio.argandonac@utem.cl>
2020-08-25 20:04:43 -0400

    Migraciones DB

---
+++ database/migrations/2020_08_25_235619_create_contenidos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContenidosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contenidos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('pedidoId');
+            $table->foreign('pedidoId')->references('id')->on('pedidos');
+            $table->unsignedBigInteger('productoId');
+            $table->foreign('productoId')->references('id')->on('productos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contenidos');
+    }
+}
