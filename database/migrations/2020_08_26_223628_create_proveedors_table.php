f0aa4c5a15769eba450668c5b7434f9e0667f17e
Germán Contreras <german.contrerasa@utem.cl>
2020-08-26 21:43:05 -0400

    Modelo y Controlador de Proveedor añadido

---
+++ database/migrations/2020_08_26_223628_create_proveedors_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProveedorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proveedores', function (Blueprint $table) {
+            $table->id();
+            $table->string('rut')->nullable();
+            $table->string('nombre');
+            $table->string('empresa')->nullable();
+            $table->string('fono');
+            $table->string('direccion')->nullable();
+            $table->string('descripcion')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proveedors');
+    }
+}
