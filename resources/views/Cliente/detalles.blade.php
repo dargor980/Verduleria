f2a14627883024ad43680763000a03b4a8677d53
Braulio Argandoña <braulio.argandonac@utem.cl>
2020-08-27 22:28:51 -0400

    Corrección Detalle Cliente

--- resources/views/Cliente/detalles.blade.php
+++ resources/views/Cliente/detalles.blade.php
@@ -8,9 +8,13 @@
     <div class="modal-content my-2">
         <br>
         <div>
+            <div class="text-right mr-5">
+                <span><a href="" ><i class="fas fa-edit text-success btn btn-light"></a></i></span>
+                <span><a href="" ><i class="fas fa-trash-alt text-danger btn btn-light"></a></i></span>
+            </div>
             <h2 class="my-2 text-white text-center">Braulio Argandoña Carrasco</h2>
         </div>
-        <div class="row my-3">
+        <div class="row my-3 mb-5">
             <div class="col-md-2"></div>
                 <div class="col-md-8">
                     <h4 class="text-white">Datos del cliente:</h4>
